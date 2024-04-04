2650993753f73694e700c78944d090076ad9a959
GT-ws <GT-websolutions@outlook.com>
2024-04-04 17:11:17 +0300

    update template home
    copying the new code in template

--- Alpha/template-home.php
+++ Alpha/template-home.php
@@ -13,6 +13,8 @@ $subtitle=get_field("hero_subtitle");
 get_template_part('templates/header');
 $aboutus=get_field("about_us");
 $aboutusfield=get_field("about_us_text");
+$aboutusimg=get_field('about_us_image');
+
 
 ?>
 <section class="hero-section" id="home" >
@@ -26,7 +28,7 @@ $aboutusfield=get_field("about_us_text");
         </div>
         <div class="col-lg-5">
           <div class="hero-image-div mx-auto">
-          <img src="images/home_fit_img.jpg" alt="">
+          <img src="<?php echo get_template_directory_uri(); ?>/assets/images/home_fit_img.jpg" alt="">
           </div>
         </div>
       </div>
@@ -38,41 +40,113 @@ $aboutusfield=get_field("about_us_text");
       <div class="row justify-content-between align-items-center">
         <div class="col-lg-7">
           <div class="d-none d-lg-block about-us-image-div">
-            <img src="images/kep.jpg" alt="">
+          <img src="<?php echo $aboutusimg; ?>" alt="">
+            
           </div>
         </div>
         <div class="col-lg-5 col-md-12 about-us-text-div">
-          <h2><?php echo $aboutus; ?> </h2>
-          <p > <?php echo $aboutusfield; ?> </p>
+          <h2><?php echo $aboutus; ?></h2>
+          <?php echo $aboutusfield; ?> 
         </div>
       </div>
     </div>
   </section>
 
   <?php
-// Check rows exists.
-if( have_rows('events') ):
+    $eventTitle = get_field('events_title');
 
-    // Loop through rows.
-    while( have_rows('events') ) : the_row();
+    if( have_rows('event_box') ):
 
-        // Load sub field value.
-       echo $event_title = get_sub_field('event_title');
+      // Loop through rows.
+      while( have_rows('event_box') ) : the_row();
 
-       echo $event_subtitle = get_sub_field('event_subtitle');
+        
+  
+          // Load sub field value.
+         $event_title = get_sub_field('event_title');
+         
+         $event_date = get_sub_field('event_date');
+  
+         $event_subtitle = get_sub_field('event_subtitle');
+  
+         $event_image = get_sub_field('event_image');
+  
+         ?>
+                 
+         
+          <?php
+      endwhile;
+  
+  // No value.
+  else :
+      // Do something...
+  endif;
+  
+  ?>
 
-       $event_image = get_sub_field('event_image')
 
-       ?>
-       <img src="<?php echo $event_image["url"];?>" alt="">
-        <?php
-    endwhile;
+  <section class="event-section" id="events">
+    <div class="container">
+      <div class="row justify-content-lg-between justify-content-md-evenly justify-content-sm-evenly justify-content-evenly">
+        <div class="col-log-12">
+          <h2 style="text-align: center; margin-top: 75px;"><?php echo $eventTitle;?></h2>
+        </div>
+        <div class="col-lg-4 col-md-6 event-card">
+          <div class="event-image-div">
+            <img src="../images/training-event.jpg" alt="">
+            <div class="card-content">
+              <h3>
+                <?php echo $event_title; ?>
+                <div class="date"><i class="fa-regular fa-calendar-days"></i> 2024.05.19</div>
+              </h3>
+              <p>
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nesciunt exercitationem iste, voluptatum, quia explicabo laboriosam rem adipisci voluptates.
+              </p>
+              <a href="" target="_blank">
+              <button class="participate" type="button">Participate</button></a>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-4 col-md-6 event-card">
+          <div class="event-image-div">
+            <img src="../images/training-event.jpg" alt="">
+            <div class="card-content">
+              <h3>
+                <?php echo $event_title; ?>
+                <div class="date"><i class="fa-regular fa-calendar-days"></i> 2024.05.19</div>
+              </h3>
+              <p>
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nesciunt exercitationem iste, voluptatum, quia explicabo laboriosam rem adipisci voluptates.
+              </p>
+              <a href="" target="_blank">
+              <button class="participate" type="button">Participate</button></a>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-4 col-md-12 event-card">
+          <div class="event-image-div">
+            <img src="../images/training-event.jpg" alt="">
+            <div class="card-content">
+              <h3>
+                Event
+                <div class="date"><i class="fa-regular fa-calendar-days"></i> 2024.05.19</div>
+              </h3>
+              <p>
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nesciunt exercitationem iste, voluptatum, quia explicabo laboriosam rem adipisci voluptates.
+              </p>
+              <a href="" target="_blank">
+              <button class="participate" type="button">Participate</button></a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
 
-// No value.
-else :
-    // Do something...
-endif;
+  </section>
+
+  <?php
+    get_template_part('templates/footer');
+    wp_footer();
+  ?>
 
 
-get_template_part('templates/footer');
- wp_footer();
